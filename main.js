519eb8a47de31734b4a058e14c60f18fb8030148
albertchitta <albert.w.chittaphong@gmail.com>
2021-08-02 22:38:24 -0500

    Created a function to sort a student into a random house

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 // Student Array
+// {
+//   name:
+//   house:
+// }
 const students = [];
 
 // Render to DOM
@@ -14,7 +18,7 @@ const welcomeBanner = () => {
       <h1 class="display-4">Welcome to Hogwarts!</h1>
       <p class="lead">We are pleased to inform you that you have a place at Hogwarts School of Witchcraft and Wizardry. Click below to see where you belong!</p>
       <p class="lead">
-        <a id="sortBtn" class="btn btn-primary btn-lg" href="#" role="button">Find My House</a>
+        <a id="startBtn" class="btn btn-primary btn-lg" href="#" role="button">Find My House</a>
       </p>
       <hr class="my-4">
     </div>
@@ -25,14 +29,14 @@ const welcomeBanner = () => {
 
 // Print the student form
 const studentForm = (event) => {
-  if (event.target.id === "sortBtn") {
+  if (event.target.id === "startBtn") {
     const domString = `
-    <form>
+    <form id="myForm">
       <div class="form-group">
         <label>Enter your name</label>
-        <input class="form-control" type="text" placeholder="Harry Potter">
+        <input id="name" class="form-control" type="text" placeholder="Harry Potter">
       </div>
-      <button type="submit" class="btn btn-primary">Sort</button>
+      <button id="sortBtn" type="submit" class="btn btn-primary">Sort</button>
     </form>
   `;
 
@@ -40,9 +44,66 @@ const studentForm = (event) => {
   }
 };
 
+// Sort student into a random house
+const studentSort = (event) => {
+  event.preventDefault();
+  if (event.target.id === "sortBtn") {
+    randomHouse = houseSort();
+
+    if (students.length === 0) {
+      studentId = 0;
+    } else {
+      studentId = students.length;
+    }
+
+    const newStudent = {
+      name: document.querySelector("#name").value,
+      house: randomHouse,
+      id: studentId
+    }
+
+    students.push(newStudent);
+    console.log(students);
+
+    studentCardBuilder(students);
+
+    // Clear form after submitting
+    document.getElementById("myForm").reset();
+  }
+};
+
+// Randomizes a house to a student in the array
+const houseSort = () => {
+  const houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
+  const house = houses[Math.floor(Math.random() * houses.length)];
+
+  return house;
+};
+
+// Builds the student card
+const studentCardBuilder = (studentsArray) => {
+  let domString = "";
+
+  studentsArray.forEach((student) => {
+    domString += `
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">${student.name}</h5>
+          <p class="card-text">${student.house}</p>
+          <button id="expelBtn" type="submit" class="btn btn-primary">Expel</button>
+        </div>
+      </div>
+    `;
+  });
+
+  renderToDom("#cardContainer", domString);
+};
+
 // Handles the button events
 const buttonEvents = () => {
   document.querySelector("#welcome").addEventListener("click", studentForm);
+  document.querySelector("#form").addEventListener("click", studentSort);
 }
 
 // Starts the app
